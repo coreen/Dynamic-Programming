e7570d2e333ef7e2b6bcf9213c8f8b6a8893aff4
coreen <coreeny@cs.washington.edu>
2014-01-08 13:34:55 -0800

    Created a basic program printing ASCII art.

---
+++ AsciiArt.py
@@ -0,0 +1,23 @@
+# Coreen Yuen
+# basic program that draws a rocker hand sign using ASCII characters
+
+print("   ___           ___")
+print("  /   \\         /   \\")
+
+# outputs repeated lines that represent the raised fingers of the hand
+for f in range(2):
+    print("  |   |         |   |")
+
+print("  |   | __   __ |   |")
+print("  |   |/  \\ /  \\|   |")
+print(" _|_  |    |    |   |")
+print("/   \\ |    |    |   |")
+print("|   | |    |    |   |")
+print("|   |  \\__/ \\__/    |")
+print("|   |               |")
+
+# outputs repeated lines that represent the palm of the hand
+for p in range(3):
+    print("|                   |")
+
+print("\\___________________/")
